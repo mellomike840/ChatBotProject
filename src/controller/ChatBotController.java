6f1dbf0ba60238d8bfabf23ea6d81a084c571af7
Alex Rottlaender <AlexRottlaender@gmail.com>
2018-10-12 12:48:28 -0600

    Created Controller, Runner, and model, added to /bin, DS_Store, and .class to .gitignore

---
+++ src/controller/ChatBotController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+public class ChatBotController
+{
+	public void start()
+	{
+		String userInput = JOptionPane.showInputDialog("What is your name?");
+		interactWithChatBot(userInput);
+		
+		
+		
+	}
+	
+	
+	/**
+	 * Method Stub
+	 * @param returnString
+	 * @return
+	 */
+	public String interactWithChatBot(String userInput)
+	{
+		//Method Stub
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/controller/ChatBotRunner.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class ChatBotRunner
+{
+
+	public static void main(String [] args)
+	{
+		ChatBotController app = new ChatBotController();
+		app.start();
+	}
+	
+	
+	
+	
+	
+}
